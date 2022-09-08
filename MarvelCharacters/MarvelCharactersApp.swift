8e1248d55493645132f24d33abeb26685c0e01a2
Alex <alexg3178@gmail.com>
2022-09-08 10:29:44 +0200

    Initial Commit

---
+++ MarvelCharacters/MarvelCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelCharactersApp.swift
+//  MarvelCharacters
+//
+//  Created by Alexandr Grigoriev on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MarvelCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
